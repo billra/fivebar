61902745007c036e18d00f8ddbc2559a793e3d53
Bill Ola Rasmussen <billra@gmail.com>
2020-06-06 16:26:46 +0200

    Point class

--- fivebar.py
+++ fivebar.py
@@ -2,8 +2,19 @@
 # Bill Ola Rasmussen
 # version 0.1
 
+class Point:
+	def __init__(self,x,y):
+		self.x=x
+		self.y=y
+	def __str__(self):
+		return "point("+str(self.x)+","+str(self.y)+")"
+
 def main():
-	print('hello world!')
+	print('Five Bar Kinematics')
+
+	p = Point(3,9)
+	print(p)
+
 	print('done.')
 
 if __name__ == "__main__":
