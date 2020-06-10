bd9cf0474e8cd71a9c26de4caf59e7944a4dd16d
Bill Ola Rasmussen <billra@gmail.com>
2020-06-10 21:19:19 +0200

    circle class

--- fivebar.py
+++ fivebar.py
@@ -4,16 +4,26 @@
 
 class Point:
 	def __init__(self,x,y):
-		self.x=x
-		self.y=y
+		self.x = x
+		self.y = y
 	def __str__(self):
-		return "point("+str(self.x)+","+str(self.y)+")"
+		return "point(" + str(self.x) + "," + str(self.y) + ")"
+
+class Circle:
+	def __init__(self,center,radius):
+		self.center = center
+		self.radius = radius
+	def __str__(self):
+		return "circle(" + str(self.center) + "," + str(self.radius) + ")"
 
 def main():
 	print('Five Bar Kinematics')
 
 	p = Point(3,9)
 	print(p)
+
+	c = Circle(Point(5,2),8)
+	print(c)
 
 	print('done.')
 
