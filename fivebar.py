15507b4e265b1b53c1d2c075521c139a6d7000cd
Bill Ola Rasmussen <billra@gmail.com>
2020-06-13 16:13:00 +0200

    convert circle_intersect to Circle member function

--- fivebar.py
+++ fivebar.py
@@ -16,29 +16,29 @@ class Circle:
 	def __str__(self):
 		return "circle(" + str(self.center) + "," + str(self.radius) + ")"
 
-def circle_intersect(x1,y1,r1,x2,y2,r2):
-	'Intersection of two circles, return empty tuple if no or infinite solutions'
-	# see: https://stackoverflow.com/a/3349134
-
-	dx = x2 - x1	# distance between circle centers on the x axis
-	dy = y2 - y1	# distance between circle centers on the y axis
-	dc = math.sqrt(dx ** 2 + dy ** 2)	# distance between circle centers
-
-	if dc > r1 + r2: return () # no solution, circles do not overlap
-	if dc < abs(r1 - r2): return () # no solution, one circle contains the other circle
-	if dc == 0 and r1 == r2: return () # infinite solutions, circles are coincident
-
-	a = (r1 ** 2 - r2 ** 2 + dc ** 2) / (2 * dc)
-	h = math.sqrt(r1 ** 2 - a ** 2)
-	xm = x1 + a * dx / dc # middle of the intersection
-	ym = y1 + a * dy / dc
-	xi1 = xm + h * dy / dc # first intersection point
-	yi1 = ym - h * dx / dc
-	if h == 0:
-		return xi1,yi1 # circles overlap at only one point
-	xi2 = xm - h * dy / dc # second intersection point
-	yi2 = ym + h * dx / dc
-	return xi1,yi1,xi2,yi2
+	def intersect(self,c2):
+		'Intersection of two circles, return empty tuple if no or infinite solutions'
+		# see: https://stackoverflow.com/a/3349134
+	
+		dx = c2.center.x - self.center.x	# distance between circle centers on the x axis
+		dy = c2.center.y - self.center.y	# distance between circle centers on the y axis
+		dc = math.sqrt(dx ** 2 + dy ** 2)	# distance between circle centers
+	
+		if dc > self.radius + c2.radius: return () # no solution, circles do not overlap
+		if dc < abs(self.radius - c2.radius): return () # no solution, one circle contains the other circle
+		if dc == 0 and self.radius == c2.radius: return () # infinite solutions, circles are coincident
+	
+		a = (self.radius ** 2 - c2.radius ** 2 + dc ** 2) / (2 * dc)
+		h = math.sqrt(self.radius ** 2 - a ** 2)
+		xm = self.center.x + a * dx / dc # middle of the intersection
+		ym = self.center.y + a * dy / dc
+		xi1 = xm + h * dy / dc # first intersection point
+		yi1 = ym - h * dx / dc
+		if h == 0:
+			return Point(xi1,yi1), # circles overlap at only one point (note: single element tuple)
+		xi2 = xm - h * dy / dc # second intersection point
+		yi2 = ym + h * dx / dc
+		return Point(xi1,yi1),Point(xi2,yi2)
 
 def main():
 	print('Five Bar Kinematics')
